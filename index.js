41fcca718db49f56e39af5a4a1c8ed5e530a5cea
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-03 16:56:22 +0300

    Completing assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// const writeCards = (names) => {
+//     return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+// }
+
+// const countDown = (number) => {
+//     for (let i = number; i >= 0; i--) {
+//         console.log(i)
+//     }
+// }
+const writeCards=(names)=>{
+    return names.map(name =>`Thank you, ${name}, for the wonderful surprise gift!`);
+}
+const countDown=(number) =>{
+    let i=number;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+
+}
