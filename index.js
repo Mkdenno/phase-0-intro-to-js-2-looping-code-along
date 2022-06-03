9946fa9d82224d10883662337a5a9d9a80a9b70a
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-03 17:03:40 +0300

    Done

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 // Code your solutions in this file
-// const writeCards = (names) => {
-//     return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
-// }
-
-// const countDown = (number) => {
-//     for (let i = number; i >= 0; i--) {
-//         console.log(i)
-//     }
-// }
 const writeCards=(names)=>{
     return names.map(name =>`Thank you, ${name}, for the wonderful surprise gift!`);
 }
